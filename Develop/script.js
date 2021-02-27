2adb620a82fb70378cebaab9ff7f90d7fd0475e5
JWalters <joshwalters34@gmail.com>
2021-02-26 21:52:29 -0500

    Added Readme documentation.  Added screenshot of my site.  Fixed the local storage for my time blocks.

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,5 @@
 var today = moment()
-
+//code to show todays date
 var todayDate = today.format("dddd, MMMM Do");
 $("#currentDay").text(todayDate);
 
@@ -10,12 +10,7 @@ var timeBlocks = document.querySelectorAll(".time-block");
 var times = document.getElementsByTagName("time");
 
 
-// function renderEvents() {
-//     var calendarEvent = JSON.parse(localStorage.getItem("event"));
-//     scheduleText.textContent = calendarEvent;
-//     return;
-// }
-
+//code to pull in calendar inputs from local storage
 $("#hour-9 .description").val(localStorage.getItem("hour-9"));
 $("#hour-10 .description").val(localStorage.getItem("hour-10"));
 $("#hour-11 .description").val(localStorage.getItem("hour-11"));
@@ -27,44 +22,16 @@ $("#hour-16 .description").val(localStorage.getItem("hour-16"));
 $("#hour-17 .description").val(localStorage.getItem("hour-17"));
 
 
-
-//create array for calendar inputs
-// function storeEvents() {
-   // var calendarText = scheduleText.value.trim();
- 
-// }
-var saveBtn = document.querySelector(".saveBtn"); //added All 
-// saveBtn[0].addEventListener("click", function() { //added [0]
-    // saveBtn.forEach((btn) => {
-saveBtn.addEventListener("click", function() { //added [0]
-    
-   // event.preventDefault();
-    // var calendarText = scheduleText.value.trim();
-    // calenderInputs.push(calendarText);
-    //   console.log(calenderInputs);
-     // storeEvents();  
-     // renderEvents();
-
-     var calendarText = $(this).siblings(".description").val();
-     var block = $(this).parent().attr("id");
-     localStorage.setItem(block, calendarText);
-
-     
-  });
-// });
-
-
-  //to pull in stored events
-//   function init(){
-    //   var calendarEvent = JSON.parse(localStorage.getItem("event"));
-    // scheduleText.innerHTML = calendarEvent;
-     // renderEvents();
-//   }
-
-  //set the background color of each time slot
-//   var currentTime = today.format("h a");
-//   console.log(currentTime);
-
+var saveBtn = document.querySelectorAll(".saveBtn"); 
+//for loop to run through each of the save buttons and then add values to local storage  
+for (i of saveBtn) {
+    i.addEventListener('click', function() {
+      var calendarText = $(this).siblings(".description").val();
+      var block = $(this).parent().attr("id");
+    localStorage.setItem(block, calendarText);
+    });    
+  };
+//function to check the current time and then compare to the time blocks
 function checkTime () {
 
     var currentTime = moment().hour();
@@ -75,25 +42,14 @@ function checkTime () {
         var thisTimeBlock = parseInt($(this).attr("id").split("-")[1]);
 
         if(thisTimeBlock < currentTime) {
-          // $(this).parent().addClass("past");
             $(this).children().addClass("past");
         } else if (thisTimeBlock === currentTime) {
-            //$(this).parent().addClass("present");
             $(this).children().addClass("present");
         } else {
-           // $(this).parent().addClass("future")
             $(this).children().addClass("future")
         }
     })
-
-    // for (var i = 0; i < 9; i++) {
-    //     moment("h a").isAfter (times[i]) 
-    //         document.getElementsByClassName("timeSlot").style.backgroundColor = "#d3d3d3";
-    //         alert("Past time");
-        
-    // }
 }
   
-//   init();
-    // console.log(times);
+//calling checkTime function 
 checkTime();
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Work-Day-Scheduler
+This website is designed to allow a user to keep track of their workday.  The user can see what hours have passed, what the current hour is, what is coming up.  Users can enter events into each hour and they are saved even when the window is refreshed.
+
+## Motivation
+My motivation for this website was to create a schedule to allow users to better maximize their days.  The color coding allows a user to easily see what hour it is and what is coming up.   
+
+### Image of my website
+
+ ![Screenshot](./Assets/Images/Scheduler_Screenshot.png)
+
+### Website Address
+
+Visit my [Website](https://joshwalters34.github.io/Work-Day-Scheduler/
+
+### Repository
+
+View my [GitHub_Repository](https://github.com/joshwalters34/Work-Day-Scheduler)
